0b572694a28f698a0768a42d833171212f5935fb
robinlrx <robin@monkey-lab.io>
2022-12-07 16:37:36 +0100

    add regex to extract number after the hastag

--- server/index.mjs
+++ server/index.mjs
@@ -27,6 +27,21 @@ io.on('connection', (socket) => {
 
 	socket.on('sendMessage', (msg) => {
 		socket.to(msg.roomId).emit('receive_message', msg);
+	
+		// console.log(msg.message) //  message
+		const specificMessage = msg.message;
+
+		if(specificMessage.includes('#')) {
+			console.log('message clé');
+			const regex = /#(\d+)/gm; // get value after the #
+
+			const hours = specificMessage.match(regex);
+			console.log('hours:', hours); // [hours]
+
+			const extractedHours = parseInt(hours[0].replace(/#/g, '')); // ex: replace # by nothing and convert string to number
+			console.log('extractedHours:', extractedHours);
+
+		}
 	});
 
 	socket.on('disconnect', () => {
